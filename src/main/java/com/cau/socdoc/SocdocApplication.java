4053023737323773240dc8a9e37b44575b1ff735
CHAE <chaso1495@naver.com>
2023-10-03 17:25:30 +0900

    first commit

---
+++ src/main/java/com/cau/socdoc/SocdocApplication.java
@@ -0,0 +1,13 @@
+package com.cau.socdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocdocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cau/socdoc/SocdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cau.socdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocdocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
