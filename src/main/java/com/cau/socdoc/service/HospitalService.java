04e83b4b8ea05698e938709cd1ce53cba01ee561
CHAE <chaso1495@naver.com>
2023-10-25 17:51:02 +0900

    [dev] 병원 관련 Service, Controller 추가 (#3)

---
+++ src/main/java/com/cau/socdoc/service/HospitalService.java
@@ -0,0 +1,15 @@
+package com.cau.socdoc.service;
+
+import com.cau.socdoc.dto.RequestAddressDto;
+import com.cau.socdoc.dto.ResponseHospitalDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HospitalService {
+
+    public List<ResponseHospitalDto> findHospitalByAddress(RequestAddressDto requestAddressDto) {
+        return null;
+    }
+}
