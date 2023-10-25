82bd9f2a305378012ea910faa8ba120d77f186c4
CHAE <chaso1495@naver.com>
2023-10-25 18:30:14 +0900

    [dev] 목록 조회 관련 dto 수정 (#3)

--- src/main/java/com/cau/socdoc/dto/RequestAddressDto.java
+++ src/main/java/com/cau/socdoc/dto/RequestAddressDto.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RequestAddressDto {
 
-    String address1;
-    String address2;
+    private String address1;
+    private String address2;
 }
--- src/main/java/com/cau/socdoc/dto/ResponseHospitalDto.java
+++ src/main/java/com/cau/socdoc/dto/ResponseHospitalDto.java
@@ -1,5 +1,36 @@
 package com.cau.socdoc.dto;
 
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class ResponseHospitalDto {
+
+    private String dutyTime3s;
+    private String dutyTime5s;
+    private String dutyTime1s;
+    private String dutyAddr;
+    private int dutyTime4c;
+    private int dutyEryn;
+    private int dutyTime6c;
+    private String hpid;
+    private double wgs84Lon;
+    private int dutyTime2c;
+    private String dutyTel1;
+    private String dutyEmclsName;
+    private String dutyTime4s;
+    private int rnum;
+    private String dutyTime6s;
+    private String dutyDivNam;
+    private String dutyTime2s;
+    private String dutyDiv;
+    private String dutyName;
+    private int dutyTime3c;
+    private int dutyTime5c;
+    private int postCdn2;
+    private String postCdn1;
+    private int dutyTime1c;
+    private double wgs84Lat;
+    private String dutyEmcls;
 }
