a54a3b1ab4a0cb7b518342ec9dd8057c6b28af9d
CHAE <chaso1495@naver.com>
2023-10-25 18:30:41 +0900

    [chore] 목록 조회 Controller 일부 수정 (#3)

--- src/main/java/com/cau/socdoc/controller/HospitalController.java
+++ src/main/java/com/cau/socdoc/controller/HospitalController.java
@@ -6,9 +6,11 @@ import com.cau.socdoc.service.HospitalService;
 import com.cau.socdoc.util.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequestMapping("/api/hospital")
@@ -20,7 +22,7 @@ public class HospitalController {
 
     // 단순 시-구 입력받아 병원 목록 조회
     @GetMapping
-    public ApiResponse<List<ResponseHospitalDto>> findHospitalByAddress(RequestAddressDto requestAddressDto) {
+    public ApiResponse<List<ResponseHospitalDto>> findHospitalByAddress(@RequestBody RequestAddressDto requestAddressDto) throws IOException {
         return ApiResponse.success(hospitalService.findHospitalByAddress(requestAddressDto), "병원 목록 조회 성공");
     }
 
