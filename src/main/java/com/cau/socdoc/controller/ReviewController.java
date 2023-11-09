e8b0d1d617e64bf75a22a0016c2dd5baa9e0c87e
CHAE <chaso1495@naver.com>
2023-11-09 13:31:06 +0900

    [dev] 유저, 리뷰 관련 API에 Swagger 적용 및 일부 경로 수정 (#10)

--- src/main/java/com/cau/socdoc/controller/ReviewController.java
+++ src/main/java/com/cau/socdoc/controller/ReviewController.java
@@ -6,6 +6,8 @@ import com.cau.socdoc.dto.response.ResponseReviewDto;
 import com.cau.socdoc.service.ReviewService;
 import com.cau.socdoc.util.api.ApiResponse;
 import com.cau.socdoc.util.api.ResponseCode;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+@Api(tags = "review")
 @RequiredArgsConstructor
 @RequestMapping("/api/review")
 @RestController
@@ -20,28 +23,38 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    // 유저의 리뷰 조회
+    @Operation(summary = "[리뷰] 유저 리뷰 조회", description = "특정 유저가 작성한 리뷰를 조회합니다.")
     @GetMapping("/read/user/{userId}")
     public ApiResponse<List<ResponseReviewDto>> readReviewByUserId(@PathVariable String userId) throws ExecutionException, InterruptedException, IOException {
         return ApiResponse.success(reviewService.readReview(userId, 0), ResponseCode.REVIEW_READ_SUCCESS.getMessage());
     }
 
+    // 병원의 리뷰 조회
+    @Operation(summary = "[리뷰] 병원 리뷰 조회", description = "특정 병원에 작성된 리뷰를 조회합니다.")
     @GetMapping("/read/hospital/{hospitalId}")
     public ApiResponse<List<ResponseReviewDto>> readReviewByHospitalId(@PathVariable String hospitalId) throws ExecutionException, InterruptedException, IOException {
         return ApiResponse.success(reviewService.readReview(hospitalId, 1), ResponseCode.REVIEW_READ_SUCCESS.getMessage());
     }
 
+    // 리뷰 생성
+    @Operation(summary = "[리뷰] 리뷰 생성", description = "리뷰를 생성합니다.")
     @PostMapping("/create")
     public ApiResponse<String> createReview(CreateReviewDto createReviewDto) throws ExecutionException, InterruptedException {
         return ApiResponse.success(reviewService.createReview(createReviewDto), ResponseCode.REVIEW_CREATE_SUCCESS.getMessage());
     }
 
+    // 리뷰 수정
+    @Operation(summary = "[리뷰] 리뷰 수정", description = "리뷰를 수정합니다.")
     @PutMapping("/update")
-    public ApiResponse<Void> updateReview(UpdateReviewDto updateReviewDto) throws Exception {
+    public ApiResponse<Void> updateReview(UpdateReviewDto updateReviewDto) throws ExecutionException, InterruptedException {
         reviewService.updateReview(updateReviewDto);
         return ApiResponse.success(null, ResponseCode.REVIEW_UPDATE_SUCCESS.getMessage());
     }
 
-    @DeleteMapping("/delete")
+    // 리뷰 삭제
+    @Operation(summary = "[리뷰] 리뷰 삭제", description = "리뷰를 삭제합니다.")
+    @DeleteMapping("/delete/reviewId/{reviewId}")
     public ApiResponse<Void> deleteReview(String reviewId) {
         reviewService.deleteReview(reviewId);
         return ApiResponse.success(null, ResponseCode.REVIEW_DELETE_SUCCESS.getMessage());
