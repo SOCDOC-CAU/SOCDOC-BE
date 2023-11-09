0ee7b116599c5700adbe7fa566224efef0621eae
CHAE <chaso1495@naver.com>
2023-11-09 13:08:39 +0900

    [dev] 전역 메시지 관리용 MessageUtil 추가 (#10)

---
+++ src/main/java/com/cau/socdoc/util/MessageUtil.java
@@ -0,0 +1,27 @@
+package com.cau.socdoc.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MessageUtil {
+
+    // 유저
+    public static final String COLLECTION_USER = "user";
+    public static final String USER_ID = "userId";
+    public static final String USER_NAME = "userName";
+    public static final String ADDRESS1 = "address1";
+    public static final String ADDRESS2 = "address2";
+
+    // 병원
+
+
+    // 리뷰
+    public static final String COLLECTION_REVIEW = "review";
+    public static final String HOSPITAL_ID = "hospitalId";
+    public static final String CONTENT = "content";
+    public static final String RATING = "rating";
+
+    // 기타
+
+}
