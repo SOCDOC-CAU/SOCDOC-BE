548506d99083c9e87b2a84f08b2988f9d7148eb9
CHAE <chaso1495@naver.com>
2023-11-11 15:15:31 +0900

    [dev] MessageUtil 보강 (#13)

--- src/main/java/com/cau/socdoc/util/MessageUtil.java
+++ src/main/java/com/cau/socdoc/util/MessageUtil.java
@@ -14,7 +14,10 @@ public class MessageUtil {
     public static final String ADDRESS2 = "address2";
 
     // 병원
-
+    public static final String COLLECTION_HOSPITAL = "hospital";
+    public static final String TYPE_HOSPITAL = "dgidIdName";
+    public static final String ADDRESS1_HOSPITAL = "address1";
+    public static final String ADDRESS2_HOSPITAL = "address2";
 
     // 리뷰
     public static final String COLLECTION_REVIEW = "review";
@@ -22,6 +25,6 @@ public class MessageUtil {
     public static final String CONTENT = "content";
     public static final String RATING = "rating";
 
-    // 기타
-
+    // 좋아요
+    public static final String COLLECTION_LIKE = "like";
 }
