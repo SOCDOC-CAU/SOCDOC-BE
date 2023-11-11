868a162c8c6778530cae64ec39c3f43761651b23
CHAE <chaso1495@naver.com>
2023-11-11 17:46:31 +0900

    [dev] Hospital 관련 누락된 요청 Dto 추가 (#13)

---
+++ src/main/java/com/cau/socdoc/dto/request/RequestSimpleHospitalDto.java
@@ -0,0 +1,11 @@
+package com.cau.socdoc.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class RequestSimpleHospitalDto {
+
+    private String address1;
+    private String address2;
+    private int pageNum;
+}
