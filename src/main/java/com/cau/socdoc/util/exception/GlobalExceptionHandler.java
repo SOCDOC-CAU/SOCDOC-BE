d56e30b8ea4dcbe78b9abfc9660c4061b695ae97
CHAE <chaso1495@naver.com>
2023-11-14 23:52:49 +0900

    [dev] RequestDto 필드별 실패원인 반환하는 예외처리 추가 및 응답객체 생성메서드 오버로딩 (#21)

--- src/main/java/com/cau/socdoc/util/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cau/socdoc/util/exception/GlobalExceptionHandler.java
@@ -1,8 +1,16 @@
 package com.cau.socdoc.util.exception;
 
 import com.cau.socdoc.util.api.ApiResponse;
+import com.cau.socdoc.util.api.ResponseCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -25,5 +33,28 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(LikeException.class)
     public ApiResponse<Void> handleLikeException(LikeException e) {
         return ApiResponse.fail(e.getResponseCode());
+    }
+
+    @ExceptionHandler(ExecutionException.class)
+    public ApiResponse<Void> handleExecutionException(ExecutionException e) {
+        return ApiResponse.fail(ResponseCode.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(InterruptedException.class)
+    public ApiResponse<Void> handleInterruptedException(InterruptedException e) {
+        return ApiResponse.fail(ResponseCode.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // 요청의 유효성 검사 실패 시
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400 Bad Request로 응답 반환
+    public ApiResponse<Map<String, String>> handleInValidRequestException(MethodArgumentNotValidException e) {
+        // 에러가 발생한 객체 내 필드와 대응하는 에러 메시지를 map에 저장하여 반환
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ApiResponse.fail(ResponseCode.BAD_REQUEST, errors);
     }
 }
