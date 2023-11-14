b8c5cda3b5a178603aa127a82e103599ce487bd5
CHAE <chaso1495@naver.com>
2023-11-14 19:02:54 +0900

    [fix] 요청Dto에 기본생성자 누락된 버그 수정 (#19)

--- src/main/java/com/cau/socdoc/dto/request/CreateUserDto.java
+++ src/main/java/com/cau/socdoc/dto/request/CreateUserDto.java
@@ -1,10 +1,10 @@
 package com.cau.socdoc.dto.request;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class CreateUserDto {
 
     private String userId;
