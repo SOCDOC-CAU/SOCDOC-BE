2b5a21620252202d3a9e34188b023ede8ae58c74
CHAE <chaso1495@naver.com>
2023-11-14 23:20:12 +0900

    [dev] 로그인 기능 @RequestHeader 누락 수정 및 로직 정리 (#21)

--- src/main/java/com/cau/socdoc/controller/UserController.java
+++ src/main/java/com/cau/socdoc/controller/UserController.java
@@ -5,6 +5,8 @@ import com.cau.socdoc.dto.request.UpdateUserAddressDto;
 import com.cau.socdoc.dto.request.UpdateUserNameDto;
 import com.cau.socdoc.dto.response.ResponseUserInfoDto;
 import com.cau.socdoc.service.UserService;
+import com.cau.socdoc.util.api.ResponseCode;
+import com.cau.socdoc.util.exception.UserException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import io.swagger.annotations.Api;
@@ -34,21 +36,25 @@ public class UserController {
     // 로그인
     @Operation(summary = "[유저] 로그인", description = "Firebase Auth idToken을 통해 유저 정보를 조회합니다. 만약 유저 정보가 없다면 유저 정보를 생성하고 정보를 반환합니다.")
     @PostMapping("/login")
-    public ResponseUserInfoDto login(String idToken) throws FirebaseAuthException, ExecutionException, InterruptedException {
+    public ResponseUserInfoDto login(@RequestHeader String firebaseToken) {
         // uid를 통해 유저 정보를 가져온다.
-        String uid = firebaseAuth.verifyIdToken(idToken).getUid();
-
-        // 유저 정보가 없다면 유저 정보를 생성한다.
-        if(userService.getUserInfo(uid) == null) {
-            userService.createUser(CreateUserDto.builder()
-                    .userId(uid)
-                    .userName(firebaseAuth.getUser(uid).getDisplayName())
-                    .userEmail(firebaseAuth.getUser(uid).getEmail())
-                    .address1("서울특별시")
-                    .address2("동작구")
-                    .build());
+        try {
+            String uid = firebaseAuth.verifyIdToken(firebaseToken).getUid();
+            if (userService.getUserInfo(uid) == null) {
+                userService.createUser(CreateUserDto.builder()
+                        .userId(uid)
+                        .userName(firebaseAuth.getUser(uid).getDisplayName())
+                        .userEmail(firebaseAuth.getUser(uid).getEmail())
+                        .address1("서울특별시")
+                        .address2("동작구")
+                        .build());
+            }
+            return userService.getUserInfo(uid);
+        } catch (FirebaseAuthException e) {
+            throw new UserException(ResponseCode.USER_NOT_FOUND);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new UserException(ResponseCode.INTERNAL_SERVER_ERROR);
         }
-        return userService.getUserInfo(uid);
     }
 
     // 유저 주소 수정
