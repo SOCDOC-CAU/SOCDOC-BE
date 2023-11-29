08b572b68ce39c1d39c9f02ff7ea21253635742c
CHAE <chaso1495@naver.com>
2023-11-29 22:21:37 +0900

    [dev] HospitalController 설명 보강 (#26)

--- src/main/java/com/cau/socdoc/controller/HospitalController.java
+++ src/main/java/com/cau/socdoc/controller/HospitalController.java
@@ -9,6 +9,7 @@ import com.cau.socdoc.service.HospitalService;
 import com.cau.socdoc.util.api.ApiResponse;
 import com.cau.socdoc.util.api.ResponseCode;
 import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,30 +26,35 @@ public class HospitalController {
     private final HospitalService hospitalService;
 
     // 병원 상세정보 조회
+    @Operation(summary = "병원 상세정보 조회", description = "특정 병원의 상세정보를 조회합니다.")
     @GetMapping("/detail/{hospitalId}")
-    public ApiResponse<ResponseDetailHospitalDto> findHospitalDetailById(@PathVariable String hospitalId) throws ExecutionException, InterruptedException {
-        return ApiResponse.success(hospitalService.findHospitalDetailById(hospitalId), ResponseCode.HOSPITAL_READ_SUCCESS.getMessage());
+    public ApiResponse<ResponseDetailHospitalDto> findHospitalDetailById(@PathVariable String hospitalId, @RequestParam String userId) throws ExecutionException, InterruptedException {
+        return ApiResponse.success(hospitalService.findHospitalDetailById(hospitalId, userId), ResponseCode.HOSPITAL_READ_SUCCESS.getMessage());
     }
 
     // 특정 지역의 특정 분과 병원 조회
+    @Operation(summary = "특정 지역의 특정 분과 병원 조회", description = "특정 지역의 특정 분과 병원 목록을 조회합니다. (예: 서울시 관악구의 치과)")
     @GetMapping("/list/{type}")
     public ApiResponse<List<ResponseSimpleHospitalDto>> findHospitalByTypeAndAddress(@PathVariable String type, @RequestBody @Valid RequestSimpleHospitalDto dto) throws ExecutionException, InterruptedException {
         return ApiResponse.success(hospitalService.findHospitalByTypeAndAddress(type, dto.getAddress1(), dto.getAddress2(), dto.getPageNum()), ResponseCode.HOSPITAL_READ_SUCCESS.getMessage());
     }
 
     // 특정 지역의 병원 조회
+    @Operation(summary = "특정 지역의 병원 조회", description = "특정 지역의 병원 목록을 조회합니다. (예: 서울시 동작구의 병원)")
     @GetMapping("/list")
     public ApiResponse<List<ResponseSimpleHospitalDto>> findHospitalByTypeAndAddress(@RequestBody @Valid RequestSimpleHospitalDto dto) throws ExecutionException, InterruptedException {
         return ApiResponse.success(hospitalService.findHospitalByAddress(dto.getAddress1(), dto.getAddress2(), dto.getPageNum()), ResponseCode.HOSPITAL_READ_SUCCESS.getMessage());
     }
 
     // 유저가 좋아요 누른 병원 조회
+    @Operation(summary = "유저가 좋아요 누른 병원 조회", description = "유저가 좋아요 누른 병원 목록을 조회합니다.")
     @GetMapping("/like/user/{userId}")
     public ApiResponse<List<ResponseSimpleHospitalDto>> findHospitalByLike(@PathVariable String userId) throws ExecutionException, InterruptedException {
         return ApiResponse.success(hospitalService.findHospitalByLike(userId), ResponseCode.HOSPITAL_READ_SUCCESS.getMessage());
     }
 
     // 유저가 병원에 좋아요 누르기
+    @Operation(summary = "유저가 병원에 좋아요 누르기", description = "유저가 특정 병원에 좋아요를 누릅니다.")
     @PostMapping("/like/user")
     public ApiResponse<Void> likeHospital(@RequestBody @Valid RequestLikeDto dto) throws ExecutionException, InterruptedException {
         hospitalService.saveLike(dto.getUserId(), dto.getHospitalId());
@@ -56,6 +62,7 @@ public class HospitalController {
     }
 
     // 유저가 병원에 좋아요 취소
+    @Operation(summary = "유저가 병원에 좋아요 취소", description = "유저가 특정 병원에 좋아요를 취소합니다.")
     @DeleteMapping("/like/user")
     public ApiResponse<Void> unlikeHospital(@RequestBody @Valid RequestLikeDto dto) throws ExecutionException, InterruptedException {
         hospitalService.deleteLike(dto.getUserId(), dto.getHospitalId());
@@ -63,6 +70,7 @@ public class HospitalController {
     }
 
     // 특정 병원 근처 약국 조회
+    @Operation(summary = "특정 병원 근처 약국 조회", description = "특정 병원 근처의 약국 목록을 조회합니다.")
     @GetMapping("/pharmacy/{hospitalId}")
     public ApiResponse<List<ResponsePharmacyDto>> findPharmacyByHospitalId(@PathVariable String hospitalId) throws ExecutionException, InterruptedException {
         return ApiResponse.success(hospitalService.findPharmacyByHospitalId(hospitalId), ResponseCode.HOSPITAL_READ_SUCCESS.getMessage());
