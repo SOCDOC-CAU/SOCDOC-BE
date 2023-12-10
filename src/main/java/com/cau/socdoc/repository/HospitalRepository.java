cc3b7dfa049f0732fe3c6c80372170afe1db5eab
CHAE <chaso1495@naver.com>
2023-12-11 04:57:47 +0900

    [fix] 병원 조회 기능 평점 내림차순, 이름 오름차순 페이징 적용 (#33)

--- src/main/java/com/cau/socdoc/repository/HospitalRepository.java
+++ src/main/java/com/cau/socdoc/repository/HospitalRepository.java
@@ -8,8 +8,8 @@ import java.util.concurrent.ExecutionException;
 public interface HospitalRepository {
 
     List<String> findAllHospital() throws ExecutionException, InterruptedException;
-    List<Hospital> findHospitalByTypeAndAddress(String type, String address1, String address2, int pageNum) throws ExecutionException, InterruptedException;
-    List<Hospital> findHospitalByAddress(String address1, String address2, int pageNum) throws ExecutionException, InterruptedException;
+    List<Hospital> findHospitalByTypeAndAddress(String type, String address1, String address2, int pageNum, int sortType) throws ExecutionException, InterruptedException;
+    List<Hospital> findHospitalByAddress(String address1, String address2, int pageNum, int sortType) throws ExecutionException, InterruptedException;
     Hospital findHospitalDetail(String hospitalId) throws ExecutionException, InterruptedException;
     List<Hospital> findHospitalByLike(String userId) throws ExecutionException, InterruptedException;
     String findNameById(String hospitalId) throws ExecutionException, InterruptedException;
